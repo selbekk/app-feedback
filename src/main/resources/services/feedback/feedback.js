5a5cbb9f68738a9ea53dbdababb86220a4df5a69
selbekk <kristofer.selbekk@bekk.no>
2016-03-11 15:21:29 -0800

    Make the feedback responses show up in the content studio

--- src/main/resources/services/feedback/feedback.js
+++ src/main/resources/services/feedback/feedback.js
@@ -1,18 +1,28 @@
 var content = require('/lib/xp/content');
+var context = require('/lib/xp/context');
 
-exports.post = function(req) {
-    var data = JSON.parse(req.body);
-
-    var entry = content.create({
-
-        parentPath: data.path + '/feedback',
-        contentType: app.name + ':feedback-item',
-        displayName: 'Feedback item',
+function saveFeedback(data) {
+    return content.create({
+        parentPath: data.path,
+        contentType: app.name + ':feedback-response',
+        displayName: 'Feedback response',
         data: {
             text: data.feedback,
             score: data.score
         }
     });
+}
+
+exports.post = function(req) {
+    var data = JSON.parse(req.body);
+
+    var entry = context.run({
+        branch: 'draft',
+        user: {
+            login: 'su',
+            userStore: 'system'
+        }
+    }, saveFeedback.bind(this, data));
 
     return {
         body: JSON.stringify(entry),
--- src/main/resources/site/parts/feedback/feedback.js
+++ src/main/resources/site/parts/feedback/feedback.js
@@ -3,13 +3,14 @@ var thymeleaf = require('/lib/xp/thymeleaf');
 
 exports.get = function (req) {
     var config = portal.getComponent().config;
-    var site = portal.getSite();
+    var content = portal.getContent();
+    log.info(JSON.stringify(content, null, 4));
 
     var view = resolve('feedback.html');
 
     var model = {
         serviceUrl: portal.serviceUrl({service: 'feedback'}),
-        path: site._path,
+        path: content._path,
         heading: config.heading,
         intro: config.intro,
         feedbackPlaceholder: config['feedback-placeholder'],
